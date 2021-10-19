9d539b2858ef42437a28f54eccaaab8d5ef32f29
Björn <frangiz@gmail.com>
2021-10-19 19:45:56 +0200

    Updated the version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="picorg",
-    version="0.1.1",
+    version="0.2.0",
     description="A script that helps you organize your pictures.",
     long_description=long_description,
     long_description_content_type="text/markdown",
