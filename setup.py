221f0cff40b2c4931a34dc677c1f85afb8350054
frangiz <frangiz@gmail.com>
2022-08-22 20:33:42 +0200

    Version 0.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="picorg",
-    version="0.2.4",
+    version="0.2.5",
     description="A script that helps you organize your pictures.",
     long_description=long_description,
     long_description_content_type="text/markdown",
