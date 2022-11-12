a20e8776856e52d581bb0da0a0c1967a29608543
Björn <frangiz@gmail.com>
2022-11-12 21:04:52 +0100

    Version 0.2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="picorg",
-    version="0.2.6",
+    version="0.2.7",
     description="A script that helps you organize your pictures.",
     long_description=long_description,
     long_description_content_type="text/markdown",
